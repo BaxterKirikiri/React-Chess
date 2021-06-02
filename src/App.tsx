90516a4bc2f1519ddc51ee457125e5c0fa64bab9
Baxter Kirikiri <baxter.kirikiri@gmail.com>
2021-06-02 17:06:30 +1200

    Added dummy login and logout functionality
    Also added local play functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,32 @@
 import React, { useState } from "react";
 import "./App.css";
-import Chessboard from "chessboardjsx";
-import { ChessInstance, ShortMove } from "chess.js";
-const Chess = require("chess.js");
+import Game from "./screens/Game";
 
 const App: React.FC = () => {
-  const [chess] = useState<ChessInstance>(
-    new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-  );
-  
-  const [fen, setFen] = useState(chess.fen());
+  const [authenticated, setAuthenticated] = useState(false);
+  const [isLocalPlay, setLocalPlay] = useState(false);
 
-  const handleMove = (move: ShortMove) =>{
-    if(chess.move(move)){
-      setTimeout(() => {
-        const moves = chess.moves();
-
-        if(moves.length > 0){
-          setFen(chess.fen());
-        }
-      }, 300);
-
-      setFen(chess.fen());
-      alertGameState();
-    }
+  if(authenticated){
+    return (
+      <div className="flex-center">
+        <Game/>
+        <button onClick={() => setAuthenticated(false)}>Logout</button>
+      </div>
+    )
+  } else if(isLocalPlay){
+    return(
+      <div className="flex-center">
+        <Game/>
+        <button onClick={() => setLocalPlay(false)}>Back</button>
+      </div>
+    )
   }
 
-  const alertGameState = () => {
-    if(chess.in_checkmate()){
-      alert("Checkmate!");
-    } else if (chess.in_stalemate()){
-      alert("Stalemate!");
-    } else if (chess.in_threefold_repetition()){
-      alert("Draw! (3 fold repitition)");
-    } else if (chess.insufficient_material()){
-      alert("Draw! (insufficient material)");
-    }
-  }
-  
-  const reset = () => {
-    chess.reset();
-    setFen(chess.fen())
-  }
 
   return (
     <div className="flex-center">
-      <h1>Baxter Chess</h1>
-      <Chessboard
-        width={400}
-        position= {fen}
-        onDrop = { (move) => 
-          handleMove({
-            from: move.sourceSquare,
-            to: move.targetSquare,
-            promotion: "q",
-          })
-      }
-      />
-      <button onClick={() => reset()}>Reset the board</button>
+      <button onClick={() => setAuthenticated(true)}>Login</button>
+      <button onClick={() => setLocalPlay(true)}>Local Play</button>
     </div>
   )
 };
---
+++ src/screens/Game.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "./Game.css";
+import Chessboard from "chessboardjsx";
+import { ChessInstance, ShortMove } from "chess.js";
+const Chess = require("chess.js");
+
+const Game: React.FC = () => {
+  const [chess] = useState<ChessInstance>(
+    new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+  );
+  
+  const [fen, setFen] = useState(chess.fen());
+
+  const handleMove = (move: ShortMove) =>{
+    if(chess.move(move)){
+      setTimeout(() => {
+        const moves = chess.moves();
+
+        if(moves.length > 0){
+          setFen(chess.fen());
+        }
+      }, 300);
+
+      setFen(chess.fen());
+      alertGameState();
+    }
+  }
+
+  const alertGameState = () => {
+    if(chess.in_checkmate()){
+      alert("Checkmate!");
+    } else if (chess.in_stalemate()){
+      alert("Stalemate!");
+    } else if (chess.in_threefold_repetition()){
+      alert("Draw! (3 fold repitition)");
+    } else if (chess.insufficient_material()){
+      alert("Draw! (insufficient material)");
+    }
+  }
+  
+  const reset = () => {
+    chess.reset();
+    setFen(chess.fen())
+  }
+
+  return (
+    <div>
+      <h1>Baxter Chess</h1>
+      <Chessboard
+        width={400}
+        position= {fen}
+        onDrop = { (move) => 
+          handleMove({
+            from: move.sourceSquare,
+            to: move.targetSquare,
+            promotion: "q",
+          })
+      }
+      />
+      <button onClick={() => reset()}>Reset the board</button>
+    </div>
+  )
+};
+
+export default Game;
