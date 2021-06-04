3bdba6c734902393add146138556ba10f39fee0a
Baxter Kirikiri <baxter.kirikiri@gmail.com>
2021-06-04 17:00:54 +1200

    Added back button to get back to the menu from the game

--- src/screens/Menu.tsx
+++ src/screens/Menu.tsx
@@ -5,10 +5,25 @@ const Menu: React.FC = () => {
     const [isLocalPLay, setLocalPlay] = useState(false);
     const [isOnlinePlay, setOnlinePlay] = useState(false);
 
+    function goBack() {
+        setLocalPlay(false);
+        setOnlinePlay(false);
+    }
+
     if(isLocalPLay){
-        return <Game/>
+        return (
+            <>
+                <Game/>
+                <button onClick={() => goBack()}>Back to Menu</button>
+            </>
+        )
     } else if(isOnlinePlay){
-        return <Game/>
+        return(
+            <>
+                <Game/>
+                <button onClick={() => goBack()}>Back to Menu</button>
+            </>
+        ) 
     }
 
     return(
